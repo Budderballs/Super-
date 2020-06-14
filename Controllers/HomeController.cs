f08546181db3360c36c70fb5fa6effbc5a2b34f7
colton chilelli <colton.chilelli@student.centralia.edu>
2020-06-13 18:21:23 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_ComicBooksCharacters.Models;
+
+namespace MVC_ComicBooksCharacters.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ComicBookContext cbcontext = null;
+        public HomeController(ComicBookContext context)
+        {
+            cbcontext = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(cbcontext);
+        }
+    }
+}
---
+++ Models/ComicBookContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace MVC_ComicBooksCharacters.Models
+{
+    public class ComicBookContext : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+
+        public ComicBookContext(DbContextOptions<ComicBookContext> options) : base(options)
+        { }
+
+        //public ComicBookContext()
+        //{ }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            //Configuration.GetConnectionString("BloggingDatabase");
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "App_Data");
+            string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDBFilename="
+                + path + "\\comicbooks.mdf; Trusted_Connection=true;MultipleActiveResultSets=true";
+
+
+            builder.UseSqlServer(conn);
+            base.OnConfiguring(builder);
+        }
+    }
+
+}
